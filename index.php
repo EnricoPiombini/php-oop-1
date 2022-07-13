b408bdc2d6e4bc665df1dd8a5e422f71c4cf527a
EnricoPiombini <piombini.enrico@gmail.com>
2022-07-13 16:06:42 +0200

    costruttore creato

--- index.php
+++ index.php
@@ -13,7 +13,17 @@
         public $author;
         public $genre;
         public $year;
+
+        function __construct($_title, $_author, $_genre, $_year)
+        {
+            $this->title = $_title;
+            $this->author = $_author;
+            $this->genre = $_genre;
+            $this->year = $_year;
+            }
  }
+
+ $movie1 = new Movie()
 
 
 
