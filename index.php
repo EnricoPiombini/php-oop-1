596cc209ee7b31d00df0c619442436eef641e3e9
EnricoPiombini <piombini.enrico@gmail.com>
2022-07-13 17:17:09 +0200

    Stampati i valori a schermo

--- index.php
+++ index.php
@@ -6,36 +6,39 @@
     - vengono istanziati almeno due oggetti ‘Movie’
      e stampati a schermo i valori delle relative proprietà -->
 
-     <?php
+    <?php
 
-     class Movie {
+    class Movie
+    {
         public $title;
-        public $author;
+        public $authorName;
+        public $authorSurname;
         public $genre;
         public $year;
+        public function getDirectorname(){
+            return $this ->authorName . " " . $this->authorSurname;
+        }
 
-        function __construct($_title, $_author, $_genre, $_year)
+        function __construct($_title, $_authorName, $_authorSurname, $_genre, $_year)
         {
             $this->title = $_title;
-            $this->author = $_author;
+            $this->authorName = $_authorName;
+            $this->authorSurname = $_authorSurname;
             $this->genre = $_genre;
             $this->year = $_year;
-            }
- }
+        }
+    };
 
- $movie1 = new Movie("Avatar", "Cameron", "Fantasy", 2009);
- //var_dump($movie1);
+    $movie1 = new Movie("Avatar", "James", "Cameron", "Fantasy", 2009);
+    //var_dump($movie1);
 
- $movie2 = new Movie("Inception", "Nolan", "ScienceDrama", 2010);
+    $movie2 = new Movie("Inception", "Cristopher", "Nolan", "ScienceDrama", 2010);
 //var_dump($movie2);
 
+    echo " Il film " . $movie1->title . " è diretto da " . $movie1->getDirectorname() . " ,è di genere " . $movie1->genre . " ed è uscito nel " . $movie1->year . ".<br>";
+    echo " Il film " . $movie2->title . " è diretto da " . $movie2->getDirectorname() . " ,è di genere " . $movie2->genre . " ed è uscito nel " . $movie2->year;
 
-
-
-
-
-
-
+    
 
 
 
